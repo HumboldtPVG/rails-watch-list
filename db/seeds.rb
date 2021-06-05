7f0a85481797d151ede8a2d4331c65c6e1c79e6b
Aaron Golec <aaron.sescie@gmail.com>
2021-06-05 15:13:27 +0800

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,16 @@ info = JSON.parse(info_serialized)
 p info
 results = info["results"]
 
-results.take(10).each do |movie|
+results.take(25).each do |movie|
   m = Movie.create!(title: movie["title"], overview: movie["overview"], poster_url: movie["poster_path"], rating: movie["vote_average"])
   puts "created a movie: #{m.title}!"
 end
 
-List.destroy_all
+# List.destroy_all
 
-puts 'creating list names'
-5.times do
-  lists = List.create!(
-    name: Faker::Hipster.word
-    )
-end
+# puts 'creating list names'
+# 5.times do
+#   lists = List.create!(
+#     name: Faker::Hipster.word
+#     )
+# end
